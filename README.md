f2975e2a90dc892a07f92fbeaa99439abd37ba2b
pramodnovus <pramod.kumar@novusinsights.com>
2025-04-04 11:01:32 +0530

    Add NavBar

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Currently, two official plugins are available:
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript and enable type-aware lint rules. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+
+
+# DevTinder
+    - Create a Vite + React application
+    - Remove unccessary code and folder and write Hello words app 
+    - Tailwind css  reference for this https://v3.tailwindcss.com/docs/guides/vite
+    - Add NavBar && Logo As well
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
 import { useState } from 'react'
 
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
-      <h1>Hello World!!!</h1>
+          <div className="navbar bg-contain shadow-lg">
+        <div className="flex-1">
+          <img
+            className='w-40 rounded-md'
+            src="/uni_invoice_logo.png"
+            alt="logo"
+          />
+        </div>
+        <div className="flex-none gap-2">
+          <div className="dropdown dropdown-end mx-4">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+      <div className='h-[96%]'>
+      <ul className="menu bg-base-200 w-56">
+        <li><a>Item 1</a></li>
+        <li><a className="active">Item 2</a></li>
+        <li><a>Item 3</a></li>
+      </ul>
+      </div>
+
+      <h1 className='font-bold text-2xl text-center pt-5 underline'>Hello World!!!</h1>
 
     </>
   )
-}
+} 
 
 export default App
